f1a8de3da9a17874197225d1629f48db94e556e4
Minh Hoàng Võ <vominhhoang12401@gmail.com>
2023-04-23 21:31:50 +0700

    add logout service

--- src/main/java/com/example/datn/configuration/SecurityConfig.java
+++ src/main/java/com/example/datn/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
   
 	private final JwtAuthenticationFilter jwtAuthFilter;
 	private final AuthenticationProvider authenticationProvider;
-//	private final LogoutHandler logoutHandler;
+	private final LogoutHandler logoutHandler;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,10 +39,10 @@ public class SecurityConfig {
 	      .and()
 	      .authenticationProvider(authenticationProvider)
 	      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-//	      .logout()
-//	      .logoutUrl("/api/v1/auth/logout")
-//	      .addLogoutHandler(logoutHandler)
-//	      .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+	      .logout()
+	      .logoutUrl("/api/v1/auth/logout")
+	      .addLogoutHandler(logoutHandler)
+	      .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
 	  ;
 	
 	  return http.build();
